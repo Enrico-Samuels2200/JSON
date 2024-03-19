e384d841a5e7cef652b8f48f789f82377c3e79fe
Enrico-Samuels2200 <enricosamuels00@gmail.com>
2024-03-19 17:57:19 +0200

    updates: 1. Uploaded project file.

---
+++ 1. DPS Resto Basic Server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const router = express.Router();
+
+// Sets up a GET request, 'http://localhost:3800/greeting'
+app.get('/greeting', (req, res) => {
+    // Returns a status code 200 and response of 'Hello World'
+    res.status(200).send('Hello World');
+});
+
+// Listens on port 3800
+server.listen(3800);
