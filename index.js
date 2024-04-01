630a85a1be81953b86c1824d1c0f590bbbd4886b
Enrico-Samuels2200 <52998518+Enrico-Samuels2200@users.noreply.github.com>
2024-04-01 18:59:18 +0200

    [updates]: Edited project to resemble basic server.

--- index.js
+++ index.js
@@ -4,41 +4,11 @@ const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
-const accounts = { "#54": {name: "someone", email: "someone@gmail.com"}};
+// Returns greeting when navigating to '/greeting' route
+app.get('/greeting', (req, res) => {
 
-// Returns all accounts
-app.get('/', (req, res) => {
-
-    // Returns a status code 200 and all accounts that exist in accounts object
-    res.status(200).send(accounts);
-});
-
-// Creates new accounts
-app.post('/create_account', (req, res) => {
-    // Makes use of params to add specific data 
-    accounts[req.query.id] = {name: req.query.name, email: req.query.email};
-    res.status(200).send(accounts);
-});
-
-// Removes accounts
-app.delete('/remove_account', (req, res) => {
-    // Removes item from JSON 
-    delete accounts[req.query.id]
-    res.status(200).send(accounts);
-});
-
-// Updates accounts
-app.put('/update_account', (req, res) => {
-    let accountData = {name: accounts[req.query.id].name, email: accounts[req.query.id].email};
-
-    // Failsafe in case user updates only name or email it doesn't remove the other row
-    // Makes use of ternary operator, if a value doesn't exist in query then it'll return the default value
-    !req.query.name ? accountData.name = accounts[req.query.id].name : accountData.name = req.query.name;
-    !req.query.email ? accountData.email = accounts[req.query.id].email : accountData.email = req.query.email;
-
-    // Updates the JSON object
-    accounts[req.query.id] = accountData;
-    res.status(200).send(accounts);
+    // Returns a status code 200 and message
+    res.status(200).send("hello);
 });
 
 // Listens on port 3800
